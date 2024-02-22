218d6b23df7d466807c1d135c7326bc3f8c8728f
Walter Berchez <50505615+Berchez@users.noreply.github.com>
2024-02-22 14:35:05 -0300

    feat: finished closeFriends. (remove dependacies later)

--- src/app/api/test/route.ts
+++ src/app/api/test/route.ts
@@ -3,6 +3,50 @@ import SteamAPI from 'steamapi';
 
 export const revalidate = 0;
 
+type UserFriend = {
+  steamID: string;
+  friendedTimestamp: number;
+  relationship: string;
+};
+
+const steam = new SteamAPI(process.env.STEAM_API_KEY ?? '');
+
+const getFriendsOfFriends = async (friendList: Array<UserFriend>) => {
+  let friendsOfFriends: Array<UserFriend> = [];
+  await Promise.all(
+    friendList.map(async (friend: UserFriend) => {
+      try {
+        const list = await steam.getUserFriends(friend.steamID);
+        friendsOfFriends.push(...list);
+      } catch (error) {}
+    }),
+  );
+
+  return friendsOfFriends;
+};
+
+const getCloseFriends = async (target: string) => {
+  const friendsOfTheTarget = await steam.getUserFriends(target);
+  const friedsOfFriendsOfTheTarget = await getFriendsOfFriends(
+    friendsOfTheTarget,
+  );
+
+  let closeFriendsOfTheTarget = await Promise.all(
+    friendsOfTheTarget.map(async (friend: UserFriend) => {
+      return {
+        friend: await steam.getUserSummary(friend.steamID),
+        count: friedsOfFriendsOfTheTarget.filter(
+          (f: UserFriend) => f.steamID === friend.steamID,
+        ).length,
+      };
+    }),
+  );
+
+  closeFriendsOfTheTarget.sort((a, b) => b.count - a.count);
+
+  return closeFriendsOfTheTarget.slice(0, 10);
+};
+
 export async function POST(req: Request) {
   if (req.method === 'POST') {
     try {
@@ -10,11 +54,10 @@ export async function POST(req: Request) {
 
       const { target } = body;
 
-      const steam = new SteamAPI(process.env.STEAM_API_KEY ?? '');
+      const targetSteamId = await steam.resolve(target);
+      const targetCloseFriends = await getCloseFriends(targetSteamId);
 
-      steam.resolve(target).then((id) => {
-        console.log('walter', id);
-      });
+      console.log('walter result', targetCloseFriends);
 
       return NextResponse.json({ message: 'Deu certo' }, { status: 200 });
     } catch (error) {
