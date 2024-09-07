88ece97088b40f61ef2603653ea24983d696f9c6
Walter Berchez <50505615+Berchez@users.noreply.github.com>
2024-09-07 15:44:09 -0300

    fix: allowing any temporarily

--- src/@types/targetInfoJsonType.ts
+++ src/@types/targetInfoJsonType.ts
@@ -3,6 +3,7 @@ import { UserSummary } from 'steamapi';
 type targetInfoJsonType =
   | {
       profileInfo: UserSummary;
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
       targetLocationInfo: any;
     }
   | undefined;
