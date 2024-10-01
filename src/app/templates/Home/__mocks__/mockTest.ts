c2f9c60593a7cd101a39d22d07a786da873e8b0e
Walter Berchez <50505615+Berchez@users.noreply.github.com>
2024-09-30 23:13:37 -0300

    feat: just getting video in prod env

---
+++ src/app/templates/Home/__mocks__/mockTest.ts
@@ -0,0 +1,160 @@
+export const mockCloseFriendsJson = [
+  {
+    friend: {
+      steamID: '76561199109858712',
+      avatar: {
+        small:
+          'https://avatars.steamstatic.com/509f90f448f3259f0f5b9597bdbbe274c8b43c40.jpg',
+        medium:
+          'https://avatars.steamstatic.com/509f90f448f3259f0f5b9597bdbbe274c8b43c40_medium.jpg',
+        large:
+          'https://avatars.steamstatic.com/509f90f448f3259f0f5b9597bdbbe274c8b43c40_full.jpg',
+        hash: '509f90f448f3259f0f5b9597bdbbe274c8b43c40',
+      },
+      url: 'https://steamcommunity.com/id/rondongostosoreidozap/',
+      profileURL: 'https://steamcommunity.com/id/rondongostosoreidozap/',
+      createdTimestamp: 1606136878,
+      realName: 'lucas',
+      nickname: 'rondon o pior',
+      primaryGroupID: '103582791429521408',
+      personaState: 0,
+      personaStateFlags: 0,
+      allowsComments: true,
+      visible: true,
+      countryCode: 'BR',
+      createdAt: new Date(),
+      lastLogOffAt: new Date(),
+    },
+    count: 3,
+    probability: 74.53333333333335,
+  },
+  {
+    friend: {
+      steamID: '76561199477973026',
+      avatar: {
+        small:
+          'https://avatars.steamstatic.com/dc02ab0a93e3011b5a28f68858a7a0ca441dda90.jpg',
+        medium:
+          'https://avatars.steamstatic.com/dc02ab0a93e3011b5a28f68858a7a0ca441dda90_medium.jpg',
+        large:
+          'https://avatars.steamstatic.com/dc02ab0a93e3011b5a28f68858a7a0ca441dda90_full.jpg',
+        hash: 'dc02ab0a93e3011b5a28f68858a7a0ca441dda90',
+      },
+      url: 'https://steamcommunity.com/profiles/76561199477973026/',
+      profileURL: 'https://steamcommunity.com/profiles/76561199477973026/',
+      createdTimestamp: 1676071111,
+      realName: 'tutu',
+      nickname: 'VERME',
+      primaryGroupID: '103582791429521408',
+      personaState: 0,
+      personaStateFlags: 0,
+      allowsComments: false,
+      visible: true,
+      countryCode: 'BR',
+      stateCode: '27',
+      createdAt: new Date(),
+      lastLogOffAt: new Date(),
+    },
+    count: 3,
+    probability: 74.53333333333335,
+  },
+  {
+    friend: {
+      steamID: '76561198153920152',
+      avatar: {
+        small:
+          'https://avatars.steamstatic.com/6330c0531c83e7262767b52a29728f6801bcc289.jpg',
+        medium:
+          'https://avatars.steamstatic.com/6330c0531c83e7262767b52a29728f6801bcc289_medium.jpg',
+        large:
+          'https://avatars.steamstatic.com/6330c0531c83e7262767b52a29728f6801bcc289_full.jpg',
+        hash: '6330c0531c83e7262767b52a29728f6801bcc289',
+      },
+      url: 'https://steamcommunity.com/id/sdhfjeuie/',
+      profileURL: 'https://steamcommunity.com/id/sdhfjeuie/',
+      createdTimestamp: 1410395077,
+      realName: 'Vitor',
+      nickname: 'lsiin',
+      primaryGroupID: '103582791429521408',
+      personaState: 1,
+      personaStateFlags: 0,
+      allowsComments: false,
+      visible: true,
+      countryCode: 'BR',
+      createdAt: new Date(),
+      lastLogOffAt: new Date(),
+    },
+    count: 2,
+    probability: 49.6888888888889,
+  },
+];
+
+export const mockPossibleLocationJson = [
+  {
+    location: {
+      cityName: 'Salvador',
+      stateName: 'Bahia',
+      countryName: 'Brazil',
+      countryCode: 'BR',
+    },
+    count: 2,
+    probability: 34,
+  },
+  {
+    location: {
+      cityName: 'Sao Paulo',
+      stateName: 'Sao Paulo',
+      countryName: 'Brazil',
+      countryCode: 'BR',
+    },
+    count: 1,
+    probability: 17,
+  },
+  {
+    location: {
+      cityName: 'Rio De Janeiro',
+      stateName: 'Rio de Janeiro',
+      countryName: 'Brazil',
+      countryCode: 'BR',
+    },
+    count: 1,
+    probability: 17,
+  },
+];
+
+export const mockTargetInfoJson = {
+  profileInfo: {
+    steamID: '76561199047191551',
+    avatar: {
+      small:
+        'https://avatars.steamstatic.com/7d9897c55597d5654a5cab91b7c03ee735558193.jpg',
+      medium:
+        'https://avatars.steamstatic.com/7d9897c55597d5654a5cab91b7c03ee735558193_medium.jpg',
+      large:
+        'https://avatars.steamstatic.com/7d9897c55597d5654a5cab91b7c03ee735558193_full.jpg',
+      hash: '7d9897c55597d5654a5cab91b7c03ee735558193',
+    },
+    url: 'https://steamcommunity.com/profiles/76561199047191551/',
+    profileURL: 'https://steamcommunity.com/profiles/76561199047191551/',
+    createdTimestamp: 1586963008,
+    lastLogOffTimestamp: 1727494561,
+    realName: 'jjao gabriel',
+    nickname: 'JJ',
+    primaryGroupID: '103582791429521408',
+    personaState: 0,
+    personaStateFlags: 0,
+    allowsComments: false,
+    visible: true,
+    countryCode: 'BR',
+    stateCode: '05',
+    cityID: '8708',
+    createdAt: new Date(),
+    lastLogOffAt: new Date(),
+  },
+  targetLocationInfo: {
+    country: {
+      code: 'BR',
+      name: 'Brazil',
+    },
+  },
+};
--- src/app/templates/Home/templates/VideoBackground/VideoBackground.tsx
+++ src/app/templates/Home/templates/VideoBackground/VideoBackground.tsx
@@ -1,18 +1,31 @@
+import Image from 'next/image';
 import React from 'react';
 
-const VideoBackground = () => (
-  <video
-    loop
-    autoPlay
-    muted
-    className="object-cover w-screen h-screen fixed top-0 left-0 z-0 brightness-90"
-    poster="/images/background.png"
-  >
-    <source
-      src="https://enuzgyrzryukbbsgtibn.supabase.co/storage/v1/object/public/video/background.mp4?t=2024-09-27T21%3A13%3A31.593Z"
-      type="video/mp4"
-    />
-  </video>
-);
+const VideoBackground = () => {
+  const env = process.env.NODE_ENV;
+  if (env === 'development') {
+    return (
+      <Image
+        src={'/images/background.png'}
+        alt="image for the background"
+        fill
+      />
+    );
+  }
 
+  return (
+    <video
+      loop
+      autoPlay
+      muted
+      className="object-cover w-screen h-screen fixed top-0 left-0 z-0 brightness-90"
+      poster="/images/background.png"
+    >
+      <source
+        src="https://enuzgyrzryukbbsgtibn.supabase.co/storage/v1/object/public/video/background.mp4?t=2024-09-27T21%3A13%3A31.593Z"
+        type="video/mp4"
+      />
+    </video>
+  );
+};
 export default VideoBackground;
