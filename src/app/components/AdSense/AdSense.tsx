59b5081d04ed8cf7b6b5053073eb41a4161d04d2
Walter Berchez <50505615+Berchez@users.noreply.github.com>
2025-02-19 22:26:30 -0300

    feat: my last try to add ad today

--- src/app/components/AdSense/AdSense.tsx
+++ src/app/components/AdSense/AdSense.tsx
@@ -1,16 +1,25 @@
-import Script from 'next/script';
-import React from 'react';
+'use client';
+import React, { useEffect } from 'react';
 
 function AdSense() {
-  return (
-    <Script
-      id="grow-me-script"
-      strategy="afterInteractive"
-      dangerouslySetInnerHTML={{
-        __html: `!(function(){window.growMe||((window.growMe=function(e){window.growMe._.push(e);}),(window.growMe._=[]));var e=document.createElement("script");(e.type="text/javascript"),(e.src="https://faves.grow.me/main.js"),(e.defer=!0),e.setAttribute("data-grow-faves-site-id","U2l0ZTpjYzRiYmVlOS1jZWU0LTRmOWItODhiNS0yMjE5Y2ZiN2QwYjU=");var t=document.getElementsByTagName("script")[0];t.parentNode.insertBefore(e,t);})();`,
-      }}
-    />
-  );
+  useEffect(() => {
+    const script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.src = 'https://faves.grow.me/main.js';
+    script.defer = true;
+    script.setAttribute(
+      'data-grow-faves-site-id',
+      'U2l0ZTpjYzRiYmVlOS1jZWU0LTRmOWItODhiNS0yMjE5Y2ZiN2QwYjU=',
+    );
+
+    document.body.appendChild(script);
+
+    return () => {
+      document.body.removeChild(script);
+    };
+  }, []);
+
+  return null;
 }
 
 export default AdSense;
