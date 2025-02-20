edb4923b7a1f5859758c242e2b1275a87f6c9790
Walter Berchez <50505615+Berchez@users.noreply.github.com>
2025-02-19 22:15:17 -0300

    feat: one more try

--- src/app/components/AdSense/AdSense.tsx
+++ src/app/components/AdSense/AdSense.tsx
@@ -5,21 +5,9 @@ function AdSense() {
   return (
     <Script
       id="grow-me-script"
-      strategy="lazyOnload"
+      strategy="afterInteractive"
       dangerouslySetInnerHTML={{
-        __html: `!(function(){
-          window.growMe||(
-            (window.growMe=function(e){ window.growMe._.push(e); }),
-            (window.growMe._=[])
-          );
-          var e=document.createElement("script");
-          e.type="text/javascript";
-          e.src="https://faves.grow.me/main.js";
-          e.defer=true;
-          e.setAttribute("data-grow-faves-site-id", "U2l0ZTpjYzRiYmVlOS1jZWU0LTRmOWItODhiNS0yMjE5Y2ZiN2QwYjU=");
-          var t=document.getElementsByTagName("script")[0];
-          t.parentNode.insertBefore(e,t);
-        })();`,
+        __html: `!(function(){window.growMe||((window.growMe=function(e){window.growMe._.push(e);}),(window.growMe._=[]));var e=document.createElement("script");(e.type="text/javascript"),(e.src="https://faves.grow.me/main.js"),(e.defer=!0),e.setAttribute("data-grow-faves-site-id","U2l0ZTpjYzRiYmVlOS1jZWU0LTRmOWItODhiNS0yMjE5Y2ZiN2QwYjU=");var t=document.getElementsByTagName("script")[0];t.parentNode.insertBefore(e,t);})();`,
       }}
     />
   );
