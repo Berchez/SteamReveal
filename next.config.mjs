dd56ce53cc1ba035725253de09adc8abe639a8af
Walter Berchez <walterfelipeberchez@outlook.com>
2025-07-07 20:31:49 -0300

    one more try to run pupeeter

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,27 @@
+// next.config.js
 import createNextIntlPlugin from 'next-intl/plugin';
 
 const withNextIntl = createNextIntlPlugin();
 
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
     domains: ['flagcdn.com', 'avatars.steamstatic.com'],
+  },
+
+  experimental: {
+    externalDir: true,
+  },
+
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.externals = [
+        ...(typeof config.externals === 'function'
+          ? []
+          : config.externals),
+        '@sparticuz/chromium',
+      ];
+    }
+    return config;
   },
 };
 
