298d7cccb293a4d68a8fc65b4b1b25395899b2e2
Walter Berchez <50505615+Berchez@users.noreply.github.com>
2025-09-28 23:23:23 -0300

    fix: improving clearStat

--- src/app/api/getCheaterProbability/utils/clearCsStats.ts
+++ src/app/api/getCheaterProbability/utils/clearCsStats.ts
@@ -1,3 +1,7 @@
-const clearStat = (stat: string) => stat.replace('ms', '').replace('%', '');
+const clearStat = (stat: string) => {
+  const cleaned = stat.replace('ms', '').replace('%', '').trim();
+
+  return cleaned === '' ? '-1' : cleaned;
+};
 
 export default clearStat;
