ceb75a66b5c583e2072c76a8aab2645876a19b97
Walter Berchez <50505615+Berchez@users.noreply.github.com>
2025-12-07 01:37:15 -0300

    refactor: removing unnecessary else

--- src/lib/getSteamApiKey.ts
+++ src/lib/getSteamApiKey.ts
@@ -2,7 +2,6 @@ export function getSteamApiKey() {
   const random = Math.random();
   if (random < 0.5) {
     return process.env.STEAM_API_KEY;
-  } else {
-    return process.env.STEAM_API_KEY_2;
   }
+  return process.env.STEAM_API_KEY_2;
 }
