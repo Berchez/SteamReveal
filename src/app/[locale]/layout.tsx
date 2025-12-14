84913b651c3413d337ed5d3432084fb97d159de7
Walter Berchez <50505615+Berchez@users.noreply.github.com>
2025-12-14 15:33:40 -0300

    refactor: removing unused imports

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -7,9 +7,6 @@ import { SpeedInsights } from '@vercel/speed-insights/next';
 import { Roboto, Inknut_Antiqua } from 'next/font/google';
 import React from 'react';
 import { headers } from 'next/headers';
-import dynamic from 'next/dynamic';
-
-const AdSense = dynamic(() => import('../components/AdSense'));
 
 const roboto = Roboto({
   subsets: ['latin'],
