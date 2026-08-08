aa909aa3576b761c352553da45ef72e006f86ed3
Walter Berchez <50505615+Berchez@users.noreply.github.com>
2026-08-08 11:51:00 -0300

    increasing timeout

--- src/app/api/getGamersClubName/route.ts
+++ src/app/api/getGamersClubName/route.ts
@@ -28,7 +28,7 @@ export async function POST(req: Request) {
         const cleanedUrl = scraperUrl.replace(/\/$/, '');
         const targetUrl = `${cleanedUrl}/api/gamersclub/${steamId}`;
         const response = await axios.get(targetUrl, {
-          timeout: 15000,
+          timeout: 30000,
         });
 
         if (response.status === 200 && response.data) {
